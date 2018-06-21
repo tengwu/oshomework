5886ccb90ced7c295f12bed8ddbcd6c32a35d9fb
wuteng <1303685031@qq.com>
2018-06-22 00:36:42 +0800

    finish mysys

--- mysys/mysys.c
+++ mysys/mysys.c
@@ -1,13 +1,36 @@
 #include <stdio.h>
+#include <sys/wait.h>
+#include <string.h>
 #include <unistd.h>
 
-int main()
-{
-    puts("before exec");
-    int error = execl("/bin/echo", "echo","a", "b", "c", NULL);
-    if(error < 0){
-        perror("execl");
+int mysys(char* cmd){
+    pid_t pid;
+    int ret = 1;
+    pid = fork();
+    if(pid < 0){
+        ret = -1;
     }
+    else if(pid == 0){
+        ret = execl("/bin/sh", "sh", "-c", cmd, NULL);
+        _exit(127);
+    }
+    else{
+        int error = wait(NULL);
+        if(error < 0){
+            printf("Some error ocured.\n");
+        }
+    }
+
+    return ret;
+}
+
+int main(int argc, char* argv[])
+{
+    printf("--------------------------------------------------\n");
+    mysys("echo HELLO WORLD");
+    printf("--------------------------------------------------\n");
+    mysys("ls /");
+    printf("--------------------------------------------------\n");
 
     return 0;
 }
