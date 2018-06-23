89f5f6c6f065f07e64aadc0089a227368137d24f
wuteng <1303685031@qq.com>
2018-06-23 14:09:26 +0800

    modify pi2

--- pi2/pi2.c
+++ pi2/pi2.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
 
 #define MAXN 1000000
-#define N 2
+#define N 100
 
-double res[N+1];
+int index_of_thread[N+1];
 
 int min(int a, int b){
 	return a < b ? a : b;
 }
 
 void *compute(void *arg){
-	int n = *(int *) arg;
+
+	double* result;
+	result = (double *)malloc(sizeof(double));
+	*result = 0;
+
+	int n = *(int *)arg;
 	int count = (MAXN + N - 1)/N;
 	int p = (n-1)*count+1;
 	int end = min(n*count, MAXN);
@@ -22,27 +28,29 @@ void *compute(void *arg){
 	if((p/2)&1 == 1)
 		flag = -1;
 	for(; p <= end; p += 2){
-		res[n] += flag*1.0/p;
+		*result += flag*1.0/p;
 		flag *= -1;
 	}
 
-	return NULL;
+	return result;
 }
 
 int main(int argc, char const *argv[])
 {
 	pthread_t thread[N+1]; // not use worker[0]
 	double ans = 0;
-	memset(res, 0, sizeof(res));
 
 	int i;
 	for(i = 1; i <= N; i++){
-		pthread_create(&thread[i], NULL, compute, &i);
-		pthread_join(thread[i], NULL);
+		index_of_thread[i] = i;
+		pthread_create(&thread[i], NULL, compute, index_of_thread+i);
 	}
-	
+
 	for(i = 1; i <= N; i++){
-		ans += res[i];
+		double *result = 0;
+		pthread_join(thread[i], (void **)&result);
+		ans += *result;
+		free(result);
 	}
 
 	printf("ans = %.8lf .\n", ans);
